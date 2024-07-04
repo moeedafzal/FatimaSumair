04703140ec9d5b1ad4f70e9683498c8deca4837d
Moeed Afzal <moeedafzal97@gmail.com>
2024-07-04 07:38:55 +0500

    sudostudy video fix

--- src/app/sudostudy/page.jsx
+++ src/app/sudostudy/page.jsx
@@ -294,7 +294,7 @@ const Page = () => {
                   height={10000}
                 />
                 <video
-                  className=" absolute top-5 left-[50%] transform -translate-x-1/2 w-full h-full"
+                  className="absolute top-2 sm:top-3 lg:top-4 left-[50%] transform -translate-x-1/2 w-full h-full"
                   controls
                   autoPlay
                   playsInline
@@ -358,7 +358,9 @@ const Page = () => {
             />
 
             <ImageContainer
-              src={"/images/work/sudostudy/page_content/final_designs/quizzes_page.png"}
+              src={
+                "/images/work/sudostudy/page_content/final_designs/quizzes_page.png"
+              }
               alt={"Quizzes details page of SudoStudy"}
             />
 
